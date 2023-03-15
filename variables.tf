90eb7f6e25ce1b399523fd3a19e3ea5c2d46e09a
Viktor Abudikhin <abudihin@gmail.com>
2023-03-15 06:49:48 +0300

    some fixes

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "zone" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "cloud_id" {
+  type    = string
+  default = "b1goe3dambm08tdcqvcb"
+}
+
+variable "folder_id" {
+  type    = string
+  default = "b1g8orq0tmhvaddbqf99"
+}
+
+variable "image_id" {
+  type = string
+
+  default = "fd8mcvr7idjrbd9kopru" # alma 8
+}
+
+variable "yc_token" {
+  type    = string
+  default = "t1.9euelZqZnceXlM2cyMeJlcaMl8bLye3rnpWans6UkJTKnpuOjYvGjI6NyZLl8_dQbWFf-e8xaBNr_d3z9xAcX1_57zFoE2v9zef1656VmsvNjZ7Ll5jJmcmWk5XGnY6Y7_0.sUaNYaWf9opQn4nM7RXrc-n5fDGqMuOl_wHFclO2s1vyR73dPH-OshxrosUHkg187ZgErEtpUFe_yZcTM9INBw"
+}
